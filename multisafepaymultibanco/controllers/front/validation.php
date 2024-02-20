e335aeed3d2bcdc2f892843447df9272de16abdd
Miguel-MultiSafepay <104830395+Miguel-MultiSafepay@users.noreply.github.com>
2024-02-20 16:57:26 +0100

    DAVAMS-683: Add Multibanco payment option (#152)

---
+++ multisafepaymultibanco/controllers/front/validation.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * MultiSafepay Payment Module
+ *
+ *  @author    MultiSafepay <integration@multisafepay.com>
+ *  @copyright Copyright (c) 2013 MultiSafepay (http://www.multisafepay.com)
+ *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ */
+require_once _PS_MODULE_DIR_ . 'multisafepay/controllers/front/validation.php';
+
+class MultisafepayMultibancoValidationModuleFrontController extends MultisafepayValidationModuleFrontController
+{
+    public function postProcess()
+    {
+        $this->type = 'redirect';
+
+        parent::postProcess();
+    }
+}
